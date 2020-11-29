be2b0b863876ba6df14b63bacaf0322cd9227f1f
Wojciech Danilo <wojciech.danilo@gmail.com>
2020-11-29 10:30:41 +0100

    refactoring

--- src/logger/src/macros.rs
+++ src/logger/src/macros.rs
@@ -9,30 +9,45 @@
 /// Internal utility for logging macros.
 #[macro_export]
 macro_rules! log_template {
-    ($level:path, $logger:expr, $msg:ident) => {
+    ($expand:ident, $level:path, $logger:expr, $msg:ident) => {
         $crate::LoggerOps::<$level>::log(&$logger,$level,$msg)
     };
 
-    ($level:path, $logger:expr, || $msg:expr) => {
-        $crate::LoggerOps::<$level>::log(&$logger,$level,|| $msg)
-    };
-
-    ($level:path, $logger:expr, $msg:tt) => {
+    ($expand:ident, $level:path, $logger:expr, $msg:tt) => {
         $crate::LoggerOps::<$level>::log(&$logger,$level,iformat!($msg))
     };
 
-    ($level:path, $logger:expr, $msg:tt, || $($body:tt)*) => {
-        {
-            $crate::LoggerOps::<$level>::group_begin(&$logger,$level,false,iformat!($msg));
-            let out = $($body)*;
-            $crate::LoggerOps::<$level>::group_end(&$logger,$level);
-            out
-        }
+    ($expand:ident, $level:path, $logger:expr, || $msg:expr) => {
+        $crate::LoggerOps::<$level>::log(&$logger,$level,|| $msg)
     };
 
-    ($level:path, $logger:expr, $msg:tt, collapsed || $($body:tt)*) => {
+    ($expand:ident, $level:path, $logger:expr, $msg:tt, $new_expand:ident || $($body:tt)*) => {
+        $crate::log_template!{$new_expand,$level,$logger,$msg,||$($body)*}
+    };
+
+    ($expand:ident, $level:path, $logger:expr, || $msg:expr, $new_expand:ident || $($body:tt)*) => {
+        $crate::log_template!{$new_expand,$level,$logger,|| $msg,||$($body)*}
+    };
+
+    ($expand:ident, $level:path, $logger:expr, $msg:ident, || $($body:tt)*) => {
+        $crate::log_template_group!($expand,$level,$logger,[$msg],||$($body)*)
+    };
+
+    ($expand:ident, $level:path, $logger:expr, $msg:tt, || $($body:tt)*) => {
+        $crate::log_template_group!($expand,$level,$logger,[iformat!($msg)],||$($body)*)
+    };
+
+    ($expand:ident, $level:path, $logger:expr, || $msg:expr, || $($body:tt)*) => {
+        $crate::log_template_group!($expand,$level,$logger,[||$msg],||$($body)*)
+    };
+}
+
+#[macro_export]
+macro_rules! log_template_group {
+    ($expand:ident, $level:path, $logger:expr, [$($msg:tt)*], || $($body:tt)*) => {
         {
-            $crate::LoggerOps::<$level>::group_begin(&$logger,$level,true,iformat!($msg));
+            $crate::LoggerOps::<$level>::group_begin
+                (&$logger,$level,$crate::collapsed_to_bool!($expand),$($msg)*);
             let out = $($body)*;
             $crate::LoggerOps::<$level>::group_end(&$logger,$level);
             out
@@ -40,29 +55,49 @@ macro_rules! log_template {
     };
 }
 
+/// Internal utility for logging macros.
+#[macro_export]
+macro_rules! collapsed_to_bool {
+    (collapsed) => { true };
+    (expanded)  => { false };
+}
 
+
+// ========================
 // === Macro Generation ===
+// ========================
 
 /// Internal utility for logging macros. Generates all the logging macros.
 ///
+/// The generated macros can be used in a variety of forms:
+/// - `warning!(logger,"literal")`, where literal will be formatted with `iformat`.
+/// - `warning!(logger,identifier)`, where identifier is a string-like variable.
+/// - `warning!(logger,|| expr)`, where expr returns a string-like variable.
+///
+/// Moreover, for each form, you can pass a third parameter. If passed, the macro will become a
+/// group, like `warning!(logger,"test",|| { ... }`. You can also use macro-keywords `collapsed`
+/// or `expanded` just before `||` to print the group collapsed or expanded by default,
+/// respectively. If not provided, the warning and error group macros are collapsed by default.
+///
+/// # Implementation Details
 /// Please note that the special pattern `$d` expands to just `$` in the generated macro from this
 /// macro.
 macro_rules! define_log_macros {
-    ($($d:tt $name:ident $tp_name:ident;)*) => {$(
+    ($($d:tt $name:ident $tp_name:ident $expand:ident;)*) => {$(
         /// $tp_name logging macro.
         #[macro_export]
         macro_rules! $name {
             ($d($d ts:tt)*) => {
-                $crate::log_template!{$crate::entry::level::$tp_name,$d($d ts)*}
+                $crate::log_template!{$expand,$crate::entry::level::$tp_name,$d($d ts)*}
             };
         }
     )*};
 }
 
 define_log_macros!{
-    $ trace   Trace;
-    $ debug   Debug;
-    $ info    Info;
-    $ warning Warning;
-    $ error   Error;
+    $ trace   Trace   expanded;
+    $ debug   Debug   expanded;
+    $ info    Info    expanded;
+    $ warning Warning collapsed;
+    $ error   Error   collapsed;
 }
